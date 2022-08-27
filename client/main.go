077d8881cd48f8a6d1ced3ef343f5c5411999357
intrpt4004 <111030717+intrpt4004-matsugasaki@users.noreply.github.com>
2022-08-27 09:21:54 +0900

    update

--- client/main.go
+++ client/main.go
@@ -3,41 +3,154 @@ package main
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
+	"net/url"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-type Time struct {
+type AuthRes struct {
+	Token   string `json:"token"`
+	Message string `json:"message"`
+}
+
+type TimeRes struct {
 	Time    string `json:"time"`
 	Message string `json:"message"`
 }
 
+type UserAgentRes struct {
+	UserAgent string `json:"user-agent"`
+	Message   string `json:"message"`
+}
+
+type ReverseRes struct {
+	Text    string `json:"text"`
+	Message string `json:"message"`
+}
+
 func main() {
-	a := app.New()
-	w := a.NewWindow("GET REST API")
-	w.Resize(fyne.NewSize(300, 200))
-	w.SetMaster()
+	app := app.New()
+	window := app.NewWindow("Client Program")
+	//	app.SetIcon(resourceIconPng)
+	window.Resize(fyne.NewSize(300, 0))
+	window.SetMaster()
 
-	w.SetContent(widget.NewButton("GET", func() {
-		res, _ := http.Get("http://localhost:3000/time")
-		defer res.Body.Close()
+	idEntry := widget.NewEntry()
+	idEntry.SetPlaceHolder("Enter your id ...")
+	pwEntry := widget.NewEntry()
+	pwEntry.SetPlaceHolder("Enter your password ...")
 
-		body, _ := io.ReadAll(res.Body)
-		var time Time
-		if err := json.Unmarshal(body, &time); err != nil {
-			log.Fatal(err)
-		}
+	loginForm := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "ID", Widget: idEntry},
+			{Text: "PW", Widget: pwEntry},
+		},
+	}
 
-		resw := a.NewWindow("RESPONSE")
-		resw.Resize(fyne.NewSize(200, 100))
-		resw.SetContent(widget.NewLabel(time.Time + " " + time.Message))
-		//		resw.SetContent(widget.NewLabel(string(body)))
-		resw.Show()
-	}))
+	tokenLabel := widget.NewLabel("")
 
-	w.ShowAndRun()
+	sepContent := widget.NewSeparator()
+	sepContent.Hide()
+
+	bodyArea := widget.NewMultiLineEntry()
+	bodyContent := container.NewMax(
+		bodyArea,
+	)
+	bodyContent.Hide()
+
+	reqContent := container.NewGridWithColumns(
+		3,
+		widget.NewButton("GET /time", func() {
+			resp, _ := http.Get("http://localhost:3000/time?token=" + tokenLabel.Text)
+			defer resp.Body.Close()
+
+			body, _ := io.ReadAll(resp.Body)
+
+			var res TimeRes
+			json.Unmarshal(body, &res)
+
+			if resp.StatusCode == http.StatusOK {
+				bodyArea.SetText(res.Time)
+			} else {
+				bodyArea.SetText(res.Message)
+			}
+		}),
+
+		widget.NewButton("GET /user-agent", func() {
+			resp, _ := http.Get("http://localhost:3000/user-agent?token=" + tokenLabel.Text)
+			defer resp.Body.Close()
+
+			body, _ := io.ReadAll(resp.Body)
+
+			var res UserAgentRes
+			json.Unmarshal(body, &res)
+
+			if resp.StatusCode == http.StatusOK {
+				bodyArea.SetText(res.UserAgent)
+			} else {
+				bodyArea.SetText(res.Message)
+			}
+		}),
+
+		widget.NewButton("POST /reverse", func() {
+			params := url.Values{}
+			params.Add("text", bodyArea.Text)
+			resp, _ := http.PostForm("http://localhost:3000/reverse?token="+tokenLabel.Text, params)
+			defer resp.Body.Close()
+
+			body, _ := io.ReadAll(resp.Body)
+
+			var res ReverseRes
+			json.Unmarshal(body, &res)
+
+			if resp.StatusCode == http.StatusOK {
+				bodyArea.SetText(res.Text)
+			} else {
+				bodyArea.SetText(res.Message)
+			}
+		}),
+	)
+	reqContent.Hide()
+
+	authContent := container.NewGridWithColumns(
+		3,
+		tokenLabel,
+		widget.NewLabel(""),
+		widget.NewButton("Auth", func() {
+			params := url.Values{}
+			params.Add("id", idEntry.Text)
+			params.Add("pw", pwEntry.Text)
+			resp, _ := http.PostForm("http://localhost:3000/auth", params)
+			defer resp.Body.Close()
+
+			body, _ := io.ReadAll(resp.Body)
+
+			var res AuthRes
+			json.Unmarshal(body, &res)
+
+			if resp.StatusCode == http.StatusOK {
+				tokenLabel.SetText(res.Token)
+
+				sepContent.Show()
+				reqContent.Show()
+				bodyContent.Show()
+			} else {
+				tokenLabel.SetText(res.Message)
+			}
+		}),
+	)
+
+	window.SetContent(container.NewVBox(
+		loginForm,
+		authContent,
+		sepContent,
+		reqContent,
+		bodyContent,
+	))
+
+	window.ShowAndRun()
 }
--- server/main.go
+++ server/main.go
@@ -7,13 +7,90 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type AuthReq struct {
+	Id string `form:"id"`
+	Pw string `form:"pw"`
+}
+
+type ReverseReq struct {
+	Text string `form:"text"`
+}
+
+const Token = "44gh348th34g93hg803g0"
+
 func main() {
 	engine := gin.Default()
+
+	engine.POST("/auth", func(c *gin.Context) {
+		req := AuthReq{}
+		c.Bind(&req)
+
+		if req.Id == "root" && req.Pw == "password" {
+			c.JSON(http.StatusOK, gin.H{
+				"token": Token,
+			})
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "failed.",
+			})
+		}
+
+	})
+
 	engine.GET("/time", func(c *gin.Context) {
+		if c.Query("token") != Token {
+			c.JSON(http.StatusForbidden, gin.H{
+				"message": "illegal token",
+			})
+
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"time":    time.Now().Format("03:04:05"),
-			"message": "hello client!",
+			"time": time.Now().Format("03:04:05"),
 		})
 	})
+
+	ua := ""
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
+	engine.GET("/user-agent", func(c *gin.Context) {
+		if c.Query("token") != Token {
+			c.JSON(http.StatusForbidden, gin.H{
+				"message": "illegal token",
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"user-agent": ua,
+		})
+	})
+
+	engine.POST("/reverse", func(c *gin.Context) {
+		if c.Query("token") != Token {
+			c.JSON(http.StatusForbidden, gin.H{
+				"message": "illegal token",
+			})
+
+			return
+		}
+
+		req := ReverseReq{}
+		c.Bind(&req)
+
+		var result []rune
+		for i := len(req.Text) - 1; i >= 0; i-- {
+			result = append(result, []rune(req.Text)[i])
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"text": string(result),
+		})
+	})
+
 	engine.Run(":3000")
 }
